7e49d22cac709db2c26208d12f94ba674a99ca39
Yufei Du <applepi0@126.com>
2014-09-14 21:44:12 +0800

    Get rid of counting in identify.js

--- utility/identify.js
+++ utility/identify.js
@@ -68,12 +68,10 @@ exports.tryIdentify = function (jsessionid, callback) {
 								});
 								response.on('end', function () {
 									var resBody = Buffer.concat(buffers, len).toString('utf8');
-									++exports.tried;
 									if (resBody.search('<title>') != -1) { // session expired
 										return callback('Session expired.', false);
 									}
 									else if (resBody.search('<valid>2</valid>') == -1) {
-										++exports.wrong;
 										/* fs.open(filePathErr, 'w', function (err, fdErr) {
 											if (!err) {
 												fs.write(fdErr, raw, 0, lenRaw, null, function (err) {
@@ -89,7 +87,6 @@ exports.tryIdentify = function (jsessionid, callback) {
 										return callback(undefined, false);
 									}
 									else {
-										++exports.correct;
 										return callback(undefined, true);
 									}
 								});
